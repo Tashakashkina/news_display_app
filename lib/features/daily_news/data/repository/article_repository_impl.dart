52441d5bce7517b78756a7f2030e68e2945252b0
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-07-14 20:30:24 +0400

    moving article obj into a daily_news folder

--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,5 +1,5 @@
 import 'package:news_display_app/core/resources/data_state.dart';
-import 'package:news_display_app/features/data/models/article.dart';
+import 'package:news_display_app/features/daily_news/data/models/article.dart';
 import 'package:news_display_app/features/domain/repository/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
