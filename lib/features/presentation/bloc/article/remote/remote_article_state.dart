0706a2034df68e76fe357819e190f61d3938deca
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-07-22 15:42:32 +0400

    add loading state to bloc (remote_article_state)

---
+++ lib/features/presentation/bloc/article/remote/remote_article_state.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+import 'package:news_display_app/features/domain/entities/article.dart';
+
+abstract class RemoteArticleState extends Equatable {
+  final List<ArticleEntity>? articles;
+  // ignore: deprecated_member_use
+  final DioError? error;
+
+  const RemoteArticleState({this.articles, this.error});
+
+  @override
+  List<Object> get props => [articles!, error!];
+}
+
+class RemoteArticlesLoading extends RemoteArticleState {
+  const RemoteArticlesLoading();
+}
