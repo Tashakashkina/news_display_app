40276090c1cd25c246af607607dd9ba71ceaf642
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-07-22 13:54:00 +0400

    create news constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = '07cc292457c64c2393201c4bc78dc7ef';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
