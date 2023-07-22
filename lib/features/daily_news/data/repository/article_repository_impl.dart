5bd82209fae6e3caa2fbeeef01689544a8103014
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-07-22 16:20:48 +0400

    add an instance of the news API service (article_repository_impl)

--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,10 +1,40 @@
+// ignore_for_file: deprecated_member_use
+
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+
+import 'package:news_display_app/core/constants/constants.dart';
 import 'package:news_display_app/core/resources/data_state.dart';
+import 'package:news_display_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:news_display_app/features/daily_news/data/models/article.dart';
 import 'package:news_display_app/features/domain/repository/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+
+  ArticleRepositoryImpl(this._newsApiService);
+
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticles() {
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: newsAPIKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(DioError(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioErrorType.badResponse,
+            requestOptions: httpResponse.response.requestOptions));
+      }
+    } on DioError catch (e) {
+      return DataFailed(e);
+    }
   }
 }
