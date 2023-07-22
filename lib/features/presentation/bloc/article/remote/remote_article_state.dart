307528f9e7cd0c635cb51bb2602c3d40365d0a99
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-07-22 15:52:22 +0400

    add completed state to bloc (remote_article_state)

--- lib/features/presentation/bloc/article/remote/remote_article_state.dart
+++ lib/features/presentation/bloc/article/remote/remote_article_state.dart
@@ -16,3 +16,8 @@ abstract class RemoteArticleState extends Equatable {
 class RemoteArticlesLoading extends RemoteArticleState {
   const RemoteArticlesLoading();
 }
+
+class RemoteArticlesDone extends RemoteArticleState {
+  const RemoteArticlesDone(List<ArticleEntity> article)
+      : super(articles: article);
+}
