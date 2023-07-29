2b5b0b44e2217a85c6969ef1d54cd8b021468c50
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-07-29 20:32:13 +0400

    register the article repo  (injection_container)

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,6 +2,8 @@ import 'package:get_it/get_it.dart';
 import 'package:news_display_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
 
 import 'package:dio/dio.dart';
+import 'package:news_display_app/features/daily_news/data/repository/article_repository_impl.dart';
+import 'package:news_display_app/features/domain/repository/article_repository.dart';
 
 final sl = GetIt.instance;
 
@@ -9,4 +11,6 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<Dio>(Dio());
 
   sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
+
+  sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl()));
 }
