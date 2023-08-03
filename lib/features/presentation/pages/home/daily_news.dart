4753ebe1ff8504e89e6ed7e192a0112c2b4381d6
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-08-03 23:03:23 +0400

    add body function to Scaffold (daily_news.dart)

--- lib/features/presentation/pages/home/daily_news.dart
+++ lib/features/presentation/pages/home/daily_news.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/article/remote/remote_article_bloc.dart';
+import '../../bloc/article/remote/remote_article_state.dart';
 
 class DailyNews extends StatelessWidget {
   const DailyNews({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _buildAppbar(),
-    );
+    return Scaffold(appBar: _buildAppbar(), body: _buildBody());
   }
 
   _buildAppbar() {
     return AppBar(
       title: const Text('Новости', style: TextStyle(color: Colors.black)),
+    );
+  }
+
+  _buildBody() {
+    return BlocBuilder<RemoteArticlesBloc, RemoteArticlesState>(
+      builder: (_, state) {
+        if (state is RemoteArticlesLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (state is RemoteArticlesError) {
+          return const Center(child: Icon(Icons.refresh));
+        }
+        if (state is RemoteArticlesDone) {
+          return ListView.builder(itemBuilder: (context, index) {
+            return const ListTile();
+          });
+        }
+        return const Placeholder();
+      },
     );
   }
 }
