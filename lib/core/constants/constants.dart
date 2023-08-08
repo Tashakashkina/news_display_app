c322a0a7e96ce009f7e5e14b46e3aaf0de391330
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-08-09 02:10:08 +0400

    add default image to ArticleModel (constants.dart)

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -2,3 +2,4 @@ const String newsAPIBaseURL = 'https://newsapi.org/v2';
 const String newsAPIKey = '07cc292457c64c2393201c4bc78dc7ef';
 const String countryQuery = 'us';
 const String categoryQuery = 'general';
+const String defaultImage = "https://asp24.ru/images/detailed/13/1_xaop-ut.png";
